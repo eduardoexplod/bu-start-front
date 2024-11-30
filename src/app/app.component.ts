11c52d8fc52afc6e6008d5618a1daa64f3c2e29a
Eduardo Mateos <eduardoexplod@gmail.com>
2024-11-30 14:12:46 -0600

    Add Template with header and Footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
-import { DialogImageComponent } from "./features/dialogs/dialog-image/dialog.image";
 
 @Component({
   selector: 'app-root',
@@ -8,23 +6,4 @@ import { DialogImageComponent } from "./features/dialogs/dialog-image/dialog.ima
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  animal: string = "";
-  name: string = "";
-
-  constructor(public dialog: MatDialog) {
-  }
-
-  openDialog(): void {
-    const dialogRef = this.dialog.open(DialogImageComponent, {
-      data: { name: this.name, animal: this.animal }, height: 'auto',
-      width: '600px',
-    },
-    );
-
-    dialogRef.afterClosed().subscribe(result => {
-      console.log('The dialog was closed');
-      this.animal = result;
-    });
-  }
-
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,24 +3,36 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+/* Angular Core */
 import { provideHttpClient } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { importProvidersFrom } from '@angular/core';
 import { MatNativeDateModule } from '@angular/material/core';
 import { FormsModule } from '@angular/forms';
 
+/* Angular Material */
 import { MatButtonModule } from "@angular/material/button";
 import { MatDialogModule } from "@angular/material/dialog";
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 
+/* Angular NGX Boostrap */
 import { ButtonsModule } from 'ngx-bootstrap/buttons';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+/* Angular Components */
+import { HeaderComponent } from './core/header/header.component';
+import { FooterComponent } from './core/footer/footer.component';
+
+/* Angular Boostrap */
+import { NgbTooltipModule } from '@ng-bootstrap/ng-bootstrap';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
@@ -30,7 +42,9 @@ import { ButtonsModule } from 'ngx-bootstrap/buttons';
     MatDialogModule,
     MatInputModule,
     MatFormFieldModule,
-    ButtonsModule.forRoot()
+    ButtonsModule.forRoot(),
+    NgbTooltipModule,
+    NgbModule
   ],
   providers: [provideHttpClient(), provideAnimations(), importProvidersFrom(MatNativeDateModule)],
   bootstrap: [AppComponent]
